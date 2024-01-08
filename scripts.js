5b05516a426a806eba13becebe48ea78ab39012b
Amalyem <ameliacallizaya328@gmail.com>
2024-01-08 11:05:27 -0400

    Lunes

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-function agregarTarea()
-{
-    var tarea = document.getElementById("nueva-tarea").value;
-    var nuevoItem = document.createElement("li");
-    nuevoItem.textContent = tarea;
-    document.getElementById("tareas-lista").appendChild ("nuevoItem");
+function agregarTarea() {
+    // Obtener el valor del input
+    var nuevaTarea = document.getElementById("nueva-tarea").value;
+
+    // Crear un nuevo elemento de lista
+    var nuevaLista = document.createElement("li");
+
+    // Asignar el valor del input al elemento de lista
+    nuevaLista.textContent = nuevaTarea;
+
+    // Agregar el nuevo elemento a la lista
+    document.getElementById("tareas-lista").appendChild(nuevaLista);
+
+    // Limpiar el valor del input
     document.getElementById("nueva-tarea").value = "";
-    
 }
