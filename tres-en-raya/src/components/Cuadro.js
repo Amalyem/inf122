ca4891155c5d7429345f397ca94e3a7b42bc76af
Amalyem <ameliacallizaya328@gmail.com>
2024-01-16 11:18:47 -0400

    Martes 16

---
+++ tres-en-raya/src/components/Cuadro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Cuadro.css';
+
+function Cuadro({ valor, funcion }) {
+    return (
+        <button className="cuadro" onClick={funcion}>
+            {valor}
+
+        </button>
+    );
+}
+
+export default Cuadro;
---
+++ tres-en-raya/src/components/Tablero.js
@@ -0,0 +1,62 @@
+import Cuadro from "./Cuadro";  
+import '../styles/Tablero.css'
+import { useState } from "react";
+function Tablero(){
+    const [cuadros, setCuadros] = useState(Array(9).fill(null));
+    const [jugador, setJugador] = useState("O");
+    const click = (i) => {
+        console.log(i);
+        console.log("turno : ", jugador);
+        const cuadrosTemp = [...cuadros];
+        cuadrosTemp[i] = jugador;
+        setCuadros(cuadrosTemp);
+        console.log(cuadrosTemp);
+        setJugador(jugador === "X"? "O":"X");
+         
+
+    }
+    /*
+    const click = (i) => {
+        const cuadrosTemp = [...cuadros];
+        cuadrosTemp[i] = jugador;
+        setCuadros(cuadrosTemp);
+        if (jugador === "X") {
+            setJugador("O");
+        } else {
+            setJugador("X");
+        }
+    }*/
+
+    /*
+    const [valor, setValor]=useState("X");
+    const click = ()=>{
+        if(valor=== "X")
+         setValor( "O"  )
+        else{
+            
+         setValor("X")
+        }
+         setValor("X":"O":"X")
+    }*/
+    return(
+        <div className='juego'>
+             <h1>Siguiente Jugador: {jugador} </h1>
+              <div className="tablero">
+                 <Cuadro valor={cuadros[0]} funcion={() => click(0)} />
+                 <Cuadro valor={cuadros[1]} funcion={() => click(1)} />
+                 <Cuadro valor={cuadros[2]} funcion={() => click(2)} />
+                 <Cuadro valor={cuadros[3]} funcion={() => click(3)} />
+                 <Cuadro valor={cuadros[4]} funcion={() => click(4)} />
+                 <Cuadro valor={cuadros[5]} funcion={() => click(5)} />
+                 <Cuadro valor={cuadros[6]} funcion={() => click(6)} />
+                 <Cuadro valor={cuadros[7]} funcion={() => click(7)} />
+                 <Cuadro valor={cuadros[8]} funcion={() => click(8)} />
+        </div>
+    </div>
+        
+        
+        
+        
+    );
+}
+export default Tablero;
