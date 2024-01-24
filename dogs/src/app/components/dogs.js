3749c4726a0cb23d9e7c0babf2b79c6e5f73b537
Amalia Pamela Callizaya <155495208+Amalyem@users.noreply.github.com>
2024-01-24 14:58:45 +0000

    Miercoles

--- dogs/src/app/components/dogs.js
+++ dogs/src/app/components/dogs.js
@@ -1,1 +1,14 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
+import style from "./dogs.module.css"
+function Dog(){
+    return(
+    const dog[dog , setDog] = useState ("/vercel.svg")
+    const url="https://dog.ceo/api/breed/rottweiler/images/random"
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=>{
+            setDog(data.message)
+
+        },[])
+    })
+    ),
+}
